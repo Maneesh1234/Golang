3947d23efa78e06e3e288703ec60beea53294518
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2020-11-12 22:24:27 +0530

    Create Interace.go
    Interface in go

---
+++ Interace.go
@@ -0,0 +1,43 @@
+// GO USE INTERFACE APPROCH TO FULFILL OOPS FEATURE BECAUSE GO DOES NOT HAVE CLASSES AND INHERITANCE
+package main
+
+import (
+	"fmt"
+)
+
+type vehicle interface {
+	accelerate()
+}
+
+func foo(v vehicle) {
+	fmt.Println(v)
+
+}
+
+type car struct {
+	model string
+	color string
+}
+
+func (c car) accelerate() {
+	fmt.Println("Accelrating?")
+
+}
+
+type toyota struct {
+	model string
+	color string
+	speed int
+}
+
+func (t toyota) accelerate() {
+	fmt.Println("I am toyota, I accelerate fast?")
+}
+func main() {
+	c1 := car{"Jeep", "Red"}
+	t1 := toyota{"Toyota", "White", 100}
+	c1.accelerate()
+	t1.accelerate()
+	foo(c1)
+	foo(t1)
+}
