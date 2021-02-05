56683ea65890fde6d4d3e60c298db14c25c2ab81
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2021-02-05 11:16:21 +0530

    Update client.go

--- Tcp_assignment/Mutiple_Client_Tcp/client.go
+++ Tcp_assignment/Mutiple_Client_Tcp/client.go
@@ -26,20 +26,22 @@ func main() {
 
 		//READ THE USER INPUT
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("CLIENT  1: ")
+		fmt.Print("CLIENT  : ")
 		text, _ := reader.ReadString('\n')
 		q = append(q, text)
 		//user input is sent to the TCP server over the network
+		fmt.Println("queue is :", q)
 		fmt.Fprintf(c, text+"\n")
 
 		//read the TCP server’s response
 		message, _ := bufio.NewReader(c).ReadString('\n')
+
 		fmt.Print("SERVER SAYS: " + message + "\n")
-		q = append(q, message)
-		fmt.Println("queue is :", q)
+		
+
 		q = q[1:]
 		fmt.Println("updated queue is :", q)
-		//q=q[1:]
+		
 		// terminate when you send the STOP command to the TCP server
 		if strings.TrimSpace(string(text)) == "STOP" {
 			fmt.Println("TCP client exiting...")
